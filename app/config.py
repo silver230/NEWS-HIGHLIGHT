8de829b1f9389e7624eef6c83c9bc28168da4d63
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-08 12:19:33 +0300

    created a configuration class for development stages of the application

--- app/config.py
+++ app/config.py
@@ -14,5 +14,13 @@ class ProdConfig(Config):
     pass
 
 
+class DevConfig(Config):
+    '''
+    Development  configuration child class
 
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+     
 
