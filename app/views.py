db1b76e3a9f31253e4fcb9dca2dc58aacb809561
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-08 11:58:03 +0300

    created a news.html file to display which i rendered onto as an argument on my views.py

--- app/views.py
+++ app/views.py
@@ -9,3 +9,11 @@ def index():
     '''
     message = 'I LOVE ANNABELLE'
     return render_template('index.html',message= message)
+
+@app.route('/news/<news_id>')
+def news(news_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('news.html',id = news_id)
