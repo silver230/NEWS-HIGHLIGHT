dbe5023c11f57b74223883bd90a8aff746365eff
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-09 21:53:51 +0300

    identified where  my sources to be gotten from

--- app/main/views.py
+++ app/main/views.py
@@ -1,22 +1,31 @@
 from flask import render_template
 from . import main
+from ..requests import get_sources,get_articles
+from ..models import Source,Article
 
+# Views
 @main.route('/')
 def index():
+    '''
+    View root page function that returns the page and and its data
+    '''
 
-    '''
-    View root page function that returns the index page and its data
-    '''
-    # Getting popular movie
-    popular_sources = get_sources('popular')
-    print(popular_sources)
-    title = 'Home - source'
-    return render_template('index.html', title=name,popular = popular_sources)
+    # Getting breaking news source
+    general_source = get_sources('general')
+    sports_source = get_sources('sports')
+    technology_source = get_sources('technology')
+    entertainment_source = get_sources('entertainment')
+    business_source = get_sources('business')
+    science_source = get_sources('science')
+    title = 'Home - Welcome to the NewsHighlights '
+    return render_template('index.html',title = title, general = general_source, sports = sports_source,technology =technology_source, entertainment = entertainment_source, business =business_source, science =science_source)
 
-@main.route('/source/<source_id>')
-def source(source_id):
+@main.route('/source/<id>')
+def articles(id):
+    '''
+    view source page function that returns the source details page and its data
+    '''
+    
+    articles = get_articles(id)
 
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('source.html',id = news_id)
+    return render_template('source.html',articles=articles)    
