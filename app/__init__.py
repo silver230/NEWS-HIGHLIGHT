f62910d0bec1bf25b184c4ab2b26a738971004ad
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-09 21:46:35 +0300

    set the configurations for the app

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,17 @@ from config import config_options
 
 def create_app(config_name):
     
-    app = Flask(__name__)
-
+    # app = Flask(__name__)
+    app = Flask(__name__,instance_relative_config = True)
     app.config.from_object(config_options[config_name])
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+    
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
 
 
     return app
