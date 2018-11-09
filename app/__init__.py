458e8a1248340c406444949437bcabb58119ad42
sylvester omondi <sylvesteromondi49@gmail.com>
2018-11-09 13:36:24 +0300

    registered my blue print

--- app/__init__.py
+++ app/__init__.py
@@ -7,5 +7,10 @@ def create_app(config_name):
 
     app.config.from_object(config_options[config_name])
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
 
     return app
